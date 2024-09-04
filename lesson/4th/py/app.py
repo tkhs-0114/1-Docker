3b41a6f347d3fe1bd09a957450aa0481575edc30
tkhs-0114 <tkhs.mail0114@gmail.com>
2024-09-04 22:43:54 +0900

    最終バージョン

--- lesson/4th/py/app.py
+++ lesson/4th/py/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World</p>"
+    return "<h1>Hello, World</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
